e5ced00af281b0bb2257dfcf464caa52ff350922
Boris Vinogradov <no111u3@gmail.com>
2019-12-15 18:52:57 +0300

    Add copy limitation to io operation types

--- src/common/io.rs
+++ src/common/io.rs
@@ -3,7 +3,7 @@
 //! Input and Output connectivity traits.
 
 /// Input provide blocking functionality for reading operations.
-pub trait Input<T> {}
+pub trait Input<T: Copy> {}
 
 /// Output provide blocking functionality for writing operations.
-pub trait Output<T> {}
+pub trait Output<T: Copy> {}
