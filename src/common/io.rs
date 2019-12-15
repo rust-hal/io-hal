a01daaa83a42dd52c60e616643d70270a9d37a19
Boris Vinogradov <no111u3@gmail.com>
2019-12-15 22:22:38 +0300

    Add draft implementation for common/io traits
    - Add main io operation to common/io traits - for one and many words

--- src/common/io.rs
+++ src/common/io.rs
@@ -3,10 +3,37 @@
 //! Input and Output connectivity traits.
 
 /// Input provide blocking functionality for reading operations.
-pub trait Input<T: Copy> {}
+pub trait Input<T: Copy> {
+    /// Input error.
+    type Error;
+
+    /// Read a single word from the input interface.
+    fn read(&mut self) -> Result<T, Self::Error>;
+
+    /// Read multiple words from the input interface to provided buffer.
+    fn reads(&mut self, buffer: &mut [T]) -> Result<(), Self::Error>;
+}
 
 /// Output provide blocking functionality for writing operations.
-pub trait Output<T: Copy> {}
+pub trait Output<T: Copy> {
+    /// Output error.
+    type Error;
+
+    /// Write a single word to the output interface.
+    fn write(&mut self, value: T) -> Result<(), Self::Error>;
+
+    /// Write multiple words to the output interface from provided buffer.
+    fn writes(&mut self, buffer: &[T]) -> Result<(), Self::Error>;
+}
 
 /// Exchange provide blocking functionality for reading and writing operations.
-pub trait Exchange<T: Copy> {}
+pub trait Exchange<T: Copy> {
+    /// Exchange error.
+    type Error;
+
+    /// Exchange a single word through the exchange interface.
+    fn exchange(&mut self, value: &mut T) -> Result<(), Self::Error>;
+
+    /// Exchange multiple words through the exchange interface from the provided buffer.
+    fn exchanges(&mut self, buffer: &mut [T]) -> Result<(), Self::Error>;
+}
