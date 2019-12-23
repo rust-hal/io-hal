c6d53c1ae268ff8380a3f9059e7d3e0d235ca4bb
Boris Vinogradov <no111u3@gmail.com>
2019-12-23 22:20:30 +0300

    Add proto traits for alternate pin mode
    - Add alternate mode enum for perephiral pin selector
    - Add alternate mode selector trait

--- src/pins/alternate.rs
+++ src/pins/alternate.rs
@@ -1,5 +1,27 @@
 //! ## Alternate pins traits.
 
+/// Alternate selector for the pin in alternate mode.
+pub enum AlternateMode {
+    /// Inter-intgrated circuit.
+    I2c(usize),
+    /// Serial peripheral interface.
+    Spi(usize),
+    /// Universal Synchronous/Asynchronous Receiver/Transmitter.
+    Usart(usize),
+    /// System timers.
+    Timer(usize),
+    /// Analog to digital converter.
+    Adc(usize),
+    /// Digital to analog converter.
+    Dac(usize),
+}
+
+/// Alternate mode configuration trait for pin.
+pub trait AlternateModePin {
+    /// set alternate mode.
+    fn set_alternate_mode(&mut self, alt_mode: AlternateMode) -> &mut Self;
+}
+
 /// Input pin trait.
 pub trait AlternateInput {}
 
