d8d39b9a4748e0b0676bf64ceb66d55515aac5e1
Boris Vinogradov <no111u3@gmail.com>
2019-12-25 21:14:12 +0300

    Add serial configuration proto
    - Add serial configuration enums - for mode and port type.
    - Add serial configuration trait proto - set mode and port type.

--- src/serial/mod.rs
+++ src/serial/mod.rs
@@ -1,3 +1,30 @@
 //! ## Serial specific part of library
 //!
 //! Operations with chip serial ports.
+
+/// Serial port type.
+pub enum SerialPortType {
+    /// Synchronous serial port - exchange data packets between reciver and transmitter
+    /// synchronously with or without syncho clock signals.
+    Synchronous,
+    /// Asynchronous serial port - exchange data packets between receiver and transmitter
+    /// asynchronously.
+    Asynchronous,
+}
+
+/// Serial port mode.
+pub enum SerialPortMode {
+    /// Full duplex mode - different lines for receiver and transmitter.
+    FullDuplex,
+    /// Half duplex mode - one line for receiver and transmitter.
+    HalfDuplex,
+}
+
+/// Common serial port configuration trait.
+pub trait SerialPortConfiguration {
+    /// Set port type.
+    fn set_type(&mut self, port_type: SerialPortType) -> &mut Self;
+
+    /// Set port mode.
+    fn set_mode(&mut self, mode: SerialPortMode) -> &mut Self;
+}
