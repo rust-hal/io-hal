6ba8a015a9c7e21652b084e80cbb1b8a5e89386a
Boris Vinogradov <no111u3@gmail.com>
2019-12-30 17:52:20 +0300

    Add device address setup for serial/i2c

--- src/serial/i2c.rs
+++ src/serial/i2c.rs
@@ -6,4 +6,7 @@
 pub trait I2cConfiguration {
     /// Set port bitrate
     fn set_port_bitrate(&mut self, bitrate: usize) -> &mut Self;
+
+    /// Set device address
+    fn set_device_address(&mut self, address: usize) -> &mut Self;
 }
