a6bc956a287de47787ae914e5d99e508b39a8a31
Boris Vinogradov <no111u3@gmail.com>
2019-12-30 15:36:22 +0300

    Add phase and polarity setup to serial/spi

--- src/serial/spi.rs
+++ src/serial/spi.rs
@@ -2,8 +2,30 @@
 //!
 //! Serial Peripheral Interface.
 
+/// Data transfer begins at
+pub enum Phase {
+    /// Forward front of the clock signals.
+    Forward,
+    /// Backward front of the clock signals.
+    BaudRate,
+}
+
+/// Clock signals polarity types.
+pub enum Polarity {
+    /// Idle state is low and pulse is high.
+    Low,
+    /// Idle state is high and pulse is low.
+    High,
+}
+
 /// Spi configuration trait.
 pub trait SpiConfiguration {
     /// Set port bitrate
     fn set_port_bitrate(&mut self, bitrate: usize) -> &mut Self;
+
+    /// Set transfer phase.
+    fn set_phase(&mut self, phase: Phase) -> &mut Self;
+
+    /// Set clock polarity.
+    fn set_polarity(&mut self, polarity: Polarity) -> &mut Self;
 }
