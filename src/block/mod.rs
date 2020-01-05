adda7a860ec0e5a40dadc3e9925be1920a6dfe4f
Boris Vinogradov <no111u3@gmail.com>
2020-01-05 21:28:16 +0300

    Add block read/write traits to block module

--- src/block/mod.rs
+++ src/block/mod.rs
@@ -7,3 +7,27 @@ pub mod flash;
 
 /// Block device configuration trait.
 pub trait BlockConfiguration {}
+
+/// Block device reader trait.
+pub trait BlockReader<T: Copy> {
+    /// Block read error
+    type Error;
+
+    /// Read one block from the device by provided address.
+    fn read_block(&mut self, address: usize) -> Result<T, Self::Error>;
+
+    /// Read many blocks from the device by provided address and buffer.
+    fn read_many_blocks(&mut self, address: usize, buffer: &mut [T]) -> Result<(), Self::Error>;
+}
+
+/// Block device writer trait.
+pub trait BlockWriter<T: Copy> {
+    /// Block write error.
+    type Error;
+
+    /// Write one block to the device by provided address.
+    fn write_block(&mut self, address: usize, block: T) -> Result<(), Self::Error>;
+
+    /// Write many blocks to the device by provided address and buffer.
+    fn write_many_blocks(&mut self, address: usize, buffer: &mut [T]) -> Result<(), Self::Error>;
+}
