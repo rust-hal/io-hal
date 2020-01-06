8bd32de0018dbb656f3de898f5605fdddfb101fa
Boris Vinogradov <no111u3@gmail.com>
2020-01-06 21:15:43 +0300

    Add block/Control and block/Erase traits
    - Add block/Control trait for setup write protection and other runtime
    options.
    - Add block/Erase trait for clearing operation of block devices.

--- src/block/mod.rs
+++ src/block/mod.rs
@@ -8,6 +8,29 @@ pub mod flash;
 /// Block device configuration trait.
 pub trait BlockConfiguration {}
 
+/// Write protection
+pub enum WriteProtect {
+    /// Write protection is enabled.
+    Enabled,
+    /// Write protection is disabled.
+    Disabled,
+}
+
+/// Block device control trait.
+pub trait BlockControl<Block: Copy> {
+    /// Block control error.
+    type Error;
+
+    /// Status of write protection.
+    fn get_write_protection_status(&self) -> Result<WriteProtect, Self::Error>;
+
+    /// Enable/Disable write protection.
+    fn set_write_protection_state(
+        &mut self,
+        write_protection: WriteProtect,
+    ) -> Result<(), Self::Error>;
+}
+
 /// Block device reader trait.
 pub trait BlockReader<Block: Copy> {
     /// Block read error
@@ -38,4 +61,16 @@ pub trait BlockWriter<Block: Copy> {
         address: *mut Block,
         buffer: &mut [Block],
     ) -> Result<(), Self::Error>;
+}
+
+/// Block device erase trait.
+pub trait BlockEraser<Block: Copy> {
+    /// Block erase error.
+    type Error;
+
+    /// Erase one block from the device by provided address.
+    fn erase_block(&mut self, address: *mut Block) -> Result<(), Self::Error>;
+
+    /// Erase many blocks from the device by provided address.
+    fn erase_many_blocks(&mut self, address: *mut Block, count: usize) -> Result<(), Self::Error>;
 }
