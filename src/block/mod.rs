3cf39ec743b8bcb39aa8f1abea6f2ffed7b6c9d2
Boris Vinogradov <no111u3@gmail.com>
2020-01-06 12:40:47 +0300

    Rename trait type from T to Block in block module

--- src/block/mod.rs
+++ src/block/mod.rs
@@ -9,25 +9,33 @@ pub mod flash;
 pub trait BlockConfiguration {}
 
 /// Block device reader trait.
-pub trait BlockReader<T: Copy> {
+pub trait BlockReader<Block: Copy> {
     /// Block read error
     type Error;
 
     /// Read one block from the device by provided address.
-    fn read_block(&mut self, address: *mut T) -> Result<T, Self::Error>;
+    fn read_block(&mut self, address: *mut Block) -> Result<Block, Self::Error>;
 
     /// Read many blocks from the device by provided address and buffer.
-    fn read_many_blocks(&mut self, address: *mut T, buffer: &mut [T]) -> Result<(), Self::Error>;
+    fn read_many_blocks(
+        &mut self,
+        address: *mut Block,
+        buffer: &mut [Block],
+    ) -> Result<(), Self::Error>;
 }
 
 /// Block device writer trait.
-pub trait BlockWriter<T: Copy> {
+pub trait BlockWriter<Block: Copy> {
     /// Block write error.
     type Error;
 
     /// Write one block to the device by provided address.
-    fn write_block(&mut self, address: *mut T, block: T) -> Result<(), Self::Error>;
+    fn write_block(&mut self, address: *mut Block, block: Block) -> Result<(), Self::Error>;
 
     /// Write many blocks to the device by provided address and buffer.
-    fn write_many_blocks(&mut self, address: *mut T, buffer: &mut [T]) -> Result<(), Self::Error>;
+    fn write_many_blocks(
+        &mut self,
+        address: *mut Block,
+        buffer: &mut [Block],
+    ) -> Result<(), Self::Error>;
 }
